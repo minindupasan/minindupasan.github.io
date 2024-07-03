c3c0ca87149c453791b9488b686c897c542a7f09
minindu-cmd <it23162532@my.sliit.lk>
2024-07-04 00:54:06 +0530

    Update read more button styling and functionality

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+const button = document.querySelector(".readmore-button");
+button.addEventListener("click", (e) => {
+  e.preventDefault;
+  button.classList.add("animate");
+  setTimeout(() => {
+    button.classList.remove("animate");
+  }, 600);
+});
+
+let readmoreButton = document.getElementById('readmore-button');
+let readmoreText = document.getElementById('readmore-text');
+
+if (readmoreButton && readmoreText) {
+  readmoreButton.addEventListener('mouseover', () => {
+    readmoreButton.style.width = '30%';
+    readmoreText.style.display = 'none';
+  });
+  readmoreButton.addEventListener('mouseout', () => {
+    readmoreButton.style.width = '5%';
+    readmoreText.style.display = 'flex';
+  });
+}
