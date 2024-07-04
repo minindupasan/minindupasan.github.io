eca93e08797ee416e0a69b2d8e0cf1110b4ef306
minindu-cmd <it23162532@my.sliit.lk>
2024-07-04 12:41:16 +0530

    Update read more button styling and functionality

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const mobileNav = document.querySelector(".hamburger");
-const navbar = document.querySelector(".menubar");
-
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
-};
-mobileNav.addEventListener("click", () => toggleNav());
-
 const button = document.querySelector(".readmore-button");
 button.addEventListener("click", (e) => {
   e.preventDefault;
@@ -15,17 +6,3 @@ button.addEventListener("click", (e) => {
     button.classList.remove("animate");
   }, 600);
 });
-
-let readmoreButton = document.getElementById('readmore-button');
-let readmoreText = document.getElementById('readmore-text');
-
-if (readmoreButton && readmoreText) {
-  readmoreButton.addEventListener('mouseover', () => {
-    readmoreButton.style.width = '30%';
-    readmoreText.style.display = 'none';
-  });
-  readmoreButton.addEventListener('mouseout', () => {
-    readmoreButton.style.width = '5%';
-    readmoreText.style.display = 'flex';
-  });
-}
