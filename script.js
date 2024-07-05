5dd992aa063106a91ade4f857dcfbeab9ccee5f6
minindu-cmd <it23162532@my.sliit.lk>
2024-07-05 14:03:14 +0530

    style: Add mousemove event listener to rotate readmore icons

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ button.addEventListener("click", (e) => {
     button.classList.remove("animate");
   }, 600);
 });
+
+document.addEventListener('mousemove',(e)=>{
+  console.log(e)
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const anchor = document.getElementById('anchor');
+    const rekt = anchor.getBoundingClientRect();
+    const anchorX = rekt.left + rekt.width/2;
+    const anchorY = rekt.top + rekt.height/2;
+
+    const angleDeg = angle(mouseX,mouseY, anchorX, anchorY);
+    console.log(angleDeg);
+
+    const rotate = document.querySelectorAll('.readmore-icon')
+    rotate.forEach((button)=>{
+      button.style.transform = `rotate(${90+angleDeg}deg)`;
+    })
+  }  
+)
+function angle(cx,cy,ex,ey){
+  const dy = ey - cy;
+  const dx = ex - cx;
+  let rad = Math.atan2(dy,dx);
+  let deg = rad * 180 / Math.PI;
+  return deg;
+}
